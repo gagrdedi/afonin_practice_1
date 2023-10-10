c818f19d5b6b39955be398c64c885f5d4f52dac3
Afonin <gagrdedi@mail.ru>
2023-10-10 22:05:20 +0400

    Added IdentityFunction test

---
+++ src/functions/SqrFunction.java
@@ -0,0 +1,9 @@
+package functions;
+import java.lang.Math;
+public class SqrFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return Math.pow(x, 2);
+    }
+}
+
