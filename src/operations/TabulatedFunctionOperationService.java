cad59198857ef7aa2b9dfd3c9a66751ea70f862f
Maccelway1 <dimaxkon358@gmail.com>
2023-11-06 22:58:52 +0400

    Edited class point

--- src/operations/TabulatedFunctionOperationService.java
+++ src/operations/TabulatedFunctionOperationService.java
@@ -2,9 +2,11 @@ package operations;
 
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
-    Point[] asPoints(TabulatedFunction tabulatedFunction){
+
+    public static Point[] asPoints(TabulatedFunction tabulatedFunction){
         int i = 0;
         Point[] p = new Point[tabulatedFunction.getCount()];
         for(Point itVar : tabulatedFunction){
