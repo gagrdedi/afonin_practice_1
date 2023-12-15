833a68827d61435970528cc29643d749ddc02ec5
Afonin <gagrdedi@mail.ru>
2023-12-15 20:12:20 +0400

    Synchronized ReadTask and WriteTask

--- src/concurrent/ReadTask.java
+++ src/concurrent/ReadTask.java
@@ -3,15 +3,17 @@ package concurrent;
 import functions.TabulatedFunction;
 
 public class ReadTask implements Runnable{
-    private TabulatedFunction function;
+    private final TabulatedFunction function;
     public ReadTask(TabulatedFunction fun) {
         function = fun;
     }
     @Override
     public void run() {
-        for (int i = 0; i < function.getCount(); i++) {
-            String string = String.format("After read: i = %d, x = %f, y = %f", i, function.getX(i), function.getY(i));
-            System.out.println(string);
+        synchronized (function) {
+            for (int i = 0; i < function.getCount(); i++) {
+                String string = String.format("After read: i = %d, x = %f, y = %f", i, function.getX(i), function.getY(i));
+                System.out.println(string);
+            }
         }
     }
 }
--- src/concurrent/WriteTask.java
+++ src/concurrent/WriteTask.java
@@ -3,18 +3,20 @@ package concurrent;
 import functions.TabulatedFunction;
 
 public class WriteTask implements Runnable{
-    private TabulatedFunction function;
-    private double value;
+    private final TabulatedFunction function;
+    private final double value;
     public WriteTask(TabulatedFunction fun, double val) {
         function = fun;
         value = val;
     }
     @Override
     public void run() {
-        for (int i = 0; i < function.getCount(); i++) {
-            function.setY(i, value);
-            String string = String.format("Writing for index %d complete", i);
-            System.out.println(string);
+        synchronized (function) {
+            for (int i = 0; i < function.getCount(); i++) {
+                function.setY(i, value);
+                String string = String.format("Writing for index %d complete", i);
+                System.out.println(string);
+            }
         }
     }
 }
