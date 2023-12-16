87ad9421be9e76a913bc2ca95a6d27f9aa4f0f97
Afonin <gagrdedi@mail.ru>
2023-12-17 00:35:17 +0400

    Fixed layout for input table

--- src/ui/TabulatedFunctionTableDialog.java
+++ src/ui/TabulatedFunctionTableDialog.java
@@ -3,6 +3,7 @@ package ui;
 import functions.factory.TabulatedFunctionFactory;
 
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,16 +13,17 @@ public class TabulatedFunctionTableDialog extends JDialog {
         super(parent, "Введите координаты точек функции", true);
         setSize(100, 30 * (size + 3));
         setLocationRelativeTo(null);
-        setLayout(new BorderLayout());
+        setResizable(false);
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
 
         JTable table = new JTable(new InputTableModel(size+1));
-        table.setSize(100, 30 * (size + 2));
+        //table.setSize(100, 30 * (size + 2));
         table.setRowHeight(30);
         table.setCellEditor(new DefaultCellEditor(new JTextField()));
         add(table);
-
         JButton button = new JButton("Подтвердить");
-        button.setSize(new Dimension(50, 30));
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //button.setSize(new Dimension(50, 30));
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
