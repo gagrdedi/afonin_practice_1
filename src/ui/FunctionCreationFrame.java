44fa9a7de9da15d83972e1ff6d843c4e94dfbf5f
Afonin <gagrdedi@mail.ru>
2023-12-16 16:43:00 +0400

    Implemented basic window

---
+++ src/ui/FunctionCreationFrame.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class FunctionCreationFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Function Creation");
+        frame.setSize(500, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
